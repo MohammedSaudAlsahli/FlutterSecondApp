ae05a23d5ce10f36d2dd5c79a3a06eae35843054
Mohammed Alsahli <mohammed.saud.alsahli@gmail.com>
2023-08-01 22:47:15 +0300

    finished assinment

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -7,48 +7,86 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              children: [
-                Image.asset(
-                  'assets/image1.png',
-                  width: 100,
-                ),
-                const Column(
-                  children: [
-                    Text('Name: Mohammed Alsahli'),
-                    Text('ID: 1010101001000101')
-                  ],
-                ),
-                Image.asset(
-                  'assets/image1.png',
-                  width: 100,
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                const Expanded(
-                  child: Text(
-                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                    overflow: TextOverflow.clip,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/image1.png',
+                    width: 100,
                   ),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                      color: Colors.red,
-                      borderRadius: BorderRadius.circular(16)),
-                  width: 200,
-                  height: 200,
-                  child: const Row(
-                    children: [],
+                  const SizedBox(width: 16),
+                  const Column(
+                    children: [
+                      Text('Name: Mohammed Alsahli'),
+                      Text('ID: 1010101001000101')
+                    ],
                   ),
-                )
-              ],
-            )
-          ],
+                  const SizedBox(width: 16),
+                  Image.asset(
+                    'assets/image1.png',
+                    width: 100,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Expanded(
+                    child: Text(
+                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 11,
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(12)),
+                    width: 200,
+                    height: 200,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text('Name: Mohammed Alsahli'),
+                              const SizedBox(height: 20),
+                              const Text('Email: fndng@gmail.com'),
+                              const SizedBox(height: 40),
+                              Row(
+                                children: [
+                                  const Expanded(
+                                    child: Text(
+                                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                                      overflow: TextOverflow.ellipsis,
+                                      maxLines: 1,
+                                    ),
+                                  ),
+                                  Image.asset(
+                                    'assets/image1.png',
+                                    width: 50,
+                                  )
+                                ],
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
